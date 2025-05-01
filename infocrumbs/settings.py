d26642dd1bac2b49d85dcde9285d901dbcce4dde
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-05-01 14:06:35 +0100

    Initial project setup with PostgreSQL, AllAuth, and env config

--- infocrumbs/settings.py
+++ infocrumbs/settings.py
@@ -15,13 +15,12 @@ import os
 import dj_database_url
 
 
-# Load env variables from env.py
-if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
-    import env
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load env variables from env.py
+if os.path.exists(os.path.join(BASE_DIR, 'env.py')):
+    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
