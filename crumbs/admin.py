d6895810aa1f3a1ce2e9baeb7c61fbcada27e3ea
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-05-15 20:36:28 +0100

    Restart the project after broken tied CustomUser

--- crumbs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Crumb
-
-@admin.register(Crumb)
-class CrumbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'created_at', 'user')
-    list_filter = ('category', 'created_at')
-    search_fields = ('title', 'content')
-    ordering = ('-created_at',)
--- crumbs/services/music.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-def authenticate_lastfm():
-    url = os.environ.get('LASTFM_API_URL')
-    try:
-        return url  # For LastFM, auth is via browser
-    except Exception:
-        return None
