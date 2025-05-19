c0f0e864b26b14988f08f2bfd3a54a1e7bf28594
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-05-19 21:35:12 +0100

    [feedback] Register SavedCrumb and LikedCrumb models in admin with enhanced display

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import SavedCrumb, LikedCrumb
 
-# Register your models here.
+@admin.register(SavedCrumb)
+class SavedCrumbAdmin(admin.ModelAdmin):
+    list_display = ('user', 'crumb', 'saved_at')
+    search_fields = ('user__username', 'crumb__title')
+    list_filter = ('saved_at',)
+
+@admin.register(LikedCrumb)
+class LikedCrumbAdmin(admin.ModelAdmin):
+    list_display = ('user', 'crumb', 'liked_at')
+    search_fields = ('user__username', 'crumb__title')
+    list_filter = ('liked_at',)
