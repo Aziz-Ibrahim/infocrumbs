4f6aaa05819dcc8fd3f323e6eef01d58fc270a09
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-05-26 00:07:26 +0100

    feat: add About page with project description, author info, and footer link
    - Created about view, template, and route in core app
    - Added project overview and author bio with external project links
    - Linked About page in site footer (base.html)
    - Added styles in base.css to enhace UX at the home page and the about page.

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('signup/', lambda request: redirect('account_signup')),
     path('login/', lambda request: redirect('account_login')),
     path('logout/', lambda request: redirect('account_logout')),
--- core/views.py
+++ core/views.py
@@ -5,3 +5,7 @@ from django.contrib.auth import logout
 # home
 def home(request):
     return render(request, 'core/home.html')
+
+# About
+def about(request):
+    return render(request, 'core/about.html')
