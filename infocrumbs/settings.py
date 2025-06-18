51c24d4b8ef40be7e1f3e5cd3c4fc12bc75a78b7
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-06-18 22:30:15 +0100

    Triggering new build to debug static files

--- infocrumbs/settings.py
+++ infocrumbs/settings.py
@@ -331,7 +331,7 @@ NEWSAPI_DIY_URL = (
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'  #Triggering new build
 
 
 # Custom user model
